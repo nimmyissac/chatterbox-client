2a2ee0ed828ed19eebee11e909b57e18ec92d083
davidank <dandahmann@gmail.com>
2016-11-11 21:34:18 -0800

    Init sending and init method for app

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,23 @@
-// YOUR CODE HERE:
+var app = {
+  init: () => {
+    
+  },
+
+  send: (message) => {
+    $.ajax({
+      url: 'https://api.parse.com/1/classes/messages',
+      type: 'POST',
+      data: JSON.stringify(message),
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
+  },
+
+
+};
