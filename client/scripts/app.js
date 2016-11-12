d9fe3cea005e61c445d7781d3ed738c7bea0f6df
davidank <dandahmann@gmail.com>
2016-11-11 21:35:48 -0800

    Init fetch method and server property of app

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,7 +1,9 @@
 var app = {
   init: () => {
-    
+
   },
+
+  server: 'https://api.parse.com/1/classes/messages',
 
   send: (message) => {
     $.ajax({
@@ -19,5 +21,18 @@ var app = {
     });
   },
 
-
+  fetch: () => {
+    $.ajax({
+      url: 'https://api.parse.com/1/classes/messages',
+      type: 'GET',
+      contentType: 'application/json',
+      success: function (data) {
+        console.log('chatterbox: Message sent');
+      },
+      error: function (data) {
+        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+        console.error('chatterbox: Failed to send message', data);
+      }
+    });
+  },
 };
